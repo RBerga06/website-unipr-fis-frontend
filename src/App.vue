17b68b3c05b333b0a832ac320313b4a17978c65c
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-12-23 00:24:52 +0100

    Add backend API connection via Axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
-import { mdiThemeLightDark, mdiWeatherNight, mdiWhiteBalanceSunny } from '@mdi/js'
+import {
+  mdiThemeLightDark,
+  mdiWeatherNight,
+  mdiWhiteBalanceSunny,
+  mdiAccount,
+  mdiAccountMultiple
+} from '@mdi/js'
 import { useTheme } from 'vuetify'
-import HelloWorld from './components/HelloWorld.vue'
 import { computed } from 'vue'
 import { ref } from 'vue'
+import axios from 'axios'
+import HelloWorld from './components/HelloWorld.vue'
+
+/* --- BACKEND API CONNECTION --- */
+const api = axios.create({
+  baseURL: 'http://127.0.0.1:8000/'
+})
+// Test the connection to the backend
+api.get('/').then((response) => {
+  console.log(response.data)
+})
+// TODO: Implement a JS/TS-only game to play if the backend is not available.
+// For example, Pong, Tetris, Snake, Mario, lichess.org's Chess Pursuit or something similar.
+// At the end of each round/game, the frontend should retry connecting to the backend.
+// If the backend becomes available, show the user a (non-enforcing) button to re-load the page.
+
+/* --- DRAWER --- */
+const drawerVisible = ref(false)
+function drawerToggle() {
+  drawerVisible.value = !drawerVisible.value
+}
 
 /* --- THEME MANAGEMENT UTILITIES --- */
 const theme = useTheme() // Vue's global theme
@@ -44,13 +70,16 @@ themeSystemApply() // Make sure we match the system theme
 
 <template>
   <v-app>
-    <v-navigation-drawer></v-navigation-drawer>
-
     <v-app-bar color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawerToggle"></v-app-bar-nav-icon>
       <v-app-bar-title>Our awesome website!</v-app-bar-title>
       <v-btn @click="themeToggle" :icon="themeIcon"></v-btn>
+      <v-btn :icon="mdiAccount"></v-btn>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawerVisible">
+      <v-list-item link title="Community" :icon="mdiAccountMultiple"></v-list-item>
+    </v-navigation-drawer>
 
     <v-container fluid>
       <v-main class="d-flex align-center justify-center">
