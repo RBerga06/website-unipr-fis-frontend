95637a77dc63b6b52b287c4bfc0525b4bdf1f8de
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-12-26 00:33:58 +0100

    AccountSummary

---
+++ src/components/AccountSummary.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { type User } from '@/stores/backend'
+
+defineProps<{
+  user: User | null
+}>()
+</script>
+
+<template>
+  <v-card>
+    <v-card-title>{{ user === null ? '[anonymous]' : user.username }}</v-card-title>
+    <v-card-subtitle>{{ user?.admin ? 'admin' : '' }}</v-card-subtitle>
+  </v-card>
+</template>
+
+<style scoped></style>
--- src/pages/CommunityPage.vue
+++ src/pages/CommunityPage.vue
@@ -1,9 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+import { useBackendStore, type User } from '@/stores/backend'
+import AccountSummary from '@/components/AccountSummary.vue'
+import { computed } from 'vue'
+
+const backend = useBackendStore()
+
+const users = ref<Array<User>>([])
+const users_admins = computed(() => {
+  return users.value.filter((u) => u.admin)
+})
+const users_nonadmins = computed(() => {
+  return users.value.filter((u) => !u.admin)
+})
+
+const refresh = ref<number | null>(null)
+onMounted(() => {
+  refresh.value = setInterval(() => {
+    backend.api
+      .get(`/users/all`)
+      .catch((_) => {
+        users.value = []
+      })
+      .then((response) => {
+        if (response) {
+          users.value = response.data as User[]
+        }
+      })
+  }, 1000) // Update every second
+})
+onUnmounted(() => {
+  if (refresh.value !== null) clearInterval(refresh.value)
+})
+</script>
 
 <template>
-  <div>
-    <h1>Community!</h1>
-  </div>
+  <v-list>
+    <v-list-subheader>Admins</v-list-subheader>
+    <v-list-item v-for="user in users_admins"
+      ><AccountSummary :user="user"></AccountSummary
+    ></v-list-item>
+    <v-list-subheader>Users</v-list-subheader>
+    <v-list-item v-for="user in users_nonadmins"
+      ><AccountSummary :user="user"></AccountSummary
+    ></v-list-item>
+  </v-list>
 </template>
 
 <style scoped></style>
